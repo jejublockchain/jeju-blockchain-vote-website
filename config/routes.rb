9c41c2ebf75ca1317c8bee501bc93ae02b3f69f4
leebeomjae <beomjae@gmail.com>
2018-04-11 06:02:28 +0000

    Main페이지와 About페이지 틀 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/main'
+	get '/about' => 'pages#about', as: 'about'
+	
+  root 'pages#main'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
