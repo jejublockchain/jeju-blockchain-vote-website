87688f79d3e8b8345ea2f5223ab0fa0d2d0d6d5b
leebeomjae <beomjae@gmail.com>
2018-04-12 00:55:17 +0000

    Devise 회원로그인화면 및 회원탈퇴기능 구현

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+	
+	has_many :identities, dependent: :destroy
   
   def self.find_for_oauth(auth, signed_in_resource = nil)
 		
